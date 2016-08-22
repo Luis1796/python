eb401389090e45fb0e03ae246059dccf80843369
Jose Luis <jluiserazod@yahoo.com.mx>
2016-08-22 12:37:36 -0500

    Segunda moficacion

--- fibonacci.py
+++ fibonacci.py
@@ -5,6 +5,15 @@ def fib_1(max):
 		print(a)
 		a ,b = b, a+b  
 
-fib_1(10)
+fib_1(10) 
 
+def fib_3(max): 
+	a,b  = 0,1 
+
+	while (a<max):
+		yield a
+		a,b =b,a+b
+
+for n in fib_3(10):
+	print(n)
 
