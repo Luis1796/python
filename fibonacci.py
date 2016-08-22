b1a0fa19208bba9b4e86af136be76ec2b95a75e9
Alumno <Alumno>
2016-08-22 12:36:08 -0500

    Nueva funcion
    Regresa lista con la serie

--- fibonacci.py
+++ fibonacci.py
@@ -5,6 +5,15 @@ def fib_1(max):
 		print(a)
 		a ,b = b, a+b  
 
+
+
+def fibo_2(max):
+	serie=[]
+	a,b=0,1
+	while a<max:
+		serie.append(a)
+		a,b=b,a+b
+	return serie
+		
 fib_1(10)
-
-
+print(fibo_3(10))
